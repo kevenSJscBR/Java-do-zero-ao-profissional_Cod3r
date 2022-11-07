b28820c512fa9e62ea4175b56984a090eb149a3d
Keven Martins <kevencardoso1@hotmail.com>
2022-11-07 10:31:57 -0300

    first commit

---
+++ Modulo/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+		//Uma sentença de codigo termina com ;
+		System.out.println("Frist Program");
+		
+		/* 
+		 * linha 1
+		 * linha 2
+		 */
+		System.out.println("Frist Program");
+
+	}
+}
